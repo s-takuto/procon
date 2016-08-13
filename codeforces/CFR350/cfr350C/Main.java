e1bced6dc7375bccff1f6d29b2faed21f5d50708
s-takuto <ts17.ngm@gmail.com>
2016-08-13 18:11:55 +0900

    ノートPCから。CFR350のC問

--- codeforces/CFR350/cfr350C/Main.java
+++ codeforces/CFR350/cfr350C/Main.java
@@ -1,6 +1,5 @@
 package cfr350C;
 
-import java.util.Comparator;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
@@ -10,6 +9,7 @@ public class Main {
 	void run() {
 		Scanner sc = new Scanner(System.in);
 
+		// input
 		int n = sc.nextInt();
 		int[] scientist = new int[n];
 		for (int i = 0; i < n; i++) {
@@ -25,27 +25,66 @@ public class Main {
 			lmovie[i] = sc.nextInt();
 		}
 
-		Map<Integer, Integer> smap = new TreeMap<Integer, Integer>();
-		Map<Integer, Integer> amap = new TreeMap<Integer, Integer>();
-		Map<Integer, Integer> lmap = new TreeMap<Integer, Integer>();
+		Map<Integer, Long> smap = new TreeMap<Integer, Long>();
 
 		for (int i = 0; i < n; i++) {
 			int key = scientist[i];
 			if (smap.size() == 0) {
-				smap.put(key, 1);
+				smap.put(key, (long) 1);
 			} else {
 				if (smap.containsKey(key)) {
-					int v = smap.get(key);
+					long v = smap.get(key);
 					smap.put(key, v + 1);
 				} else {
-					smap.put(key, 1);
+					smap.put(key, (long) 1);
 				}
 			}
 		}
 
-		for (int i = 0; i < m; i++) {
+		// debug
+		// for (Integer key : smap.keySet()) {
+		// System.out.println(key + " " + smap.get(key));
+		// }
 
+		long amax = -1;
+		long lmax = -1;
+		int index = 0;
+		for (int i = 0; i < m; i++) {
+			int akey = amovie[i];
+			int lkey = lmovie[i];
+
+			if (!smap.containsKey(akey)) {
+				if (!smap.containsKey(lkey) || amax != -1) {
+					continue;
+				}
+				long w = smap.get(lkey);
+				if (lmax < w) {
+					lmax = w;
+					index = i;
+				}
+				continue;
+			}
+
+			long v = smap.get(akey);
+			if (amax < v) {
+				amax = v;
+				if (smap.containsKey(lkey)) {
+					long w = smap.get(lkey);
+					lmax = w;
+				}
+				index = i;
+			} else if (v == amax) {
+				if (smap.containsKey(lkey)) {
+					long w = smap.get(lkey);
+					if (lmax < w) {
+						lmax = w;
+						index = i;
+					}
+				}
+			}
 		}
+
+		System.out.println(index + 1);
 
 	}
 
