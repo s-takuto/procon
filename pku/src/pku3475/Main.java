41c9ad9197ffe918e2d0d860190936b195f0e4bf
s-takuto <ts17.ngm@gmail.com>
2016-10-28 18:16:56 +0900

    pku3475

--- pku/src/pku3475/Main.java
+++ pku/src/pku3475/Main.java
@@ -4,44 +4,42 @@ import java.util.Scanner;
 
 public class Main {
 
+	public static int solve(int a, int b, int c, int d) {
+		int count = 0;
+		while (a < c) {
+			if (c % 2 != 0) {
+				c /= 2;
+				c += 1;
+			} else {
+				c /= 2;
+			}
+			count++;
+		}
+		while (b < d) {
+			if (d % 2 != 0) {
+				d /= 2;
+				d += 1;
+			} else {
+				d /= 2;
+			}
+			count++;
+		}
+		return count;
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while (sc.hasNext()) {
+			// 封筒の縦a, 横b
 			int a = sc.nextInt();
 			int b = sc.nextInt();
+			// 紙の縦c, 横d
 			int c = sc.nextInt();
 			int d = sc.nextInt();
 
-			int count = 0;
-			int min = Integer.MAX_VALUE / 2;
-
-			int tmpc = c;
-			int tmpd = d;
-
-			while (a < c) {
-				c /= 2;
-				count++;
-			}
-			while (b < d) {
-				d /= 2;
-				count++;
-			}
-			
-			min = count;
-			c = tmpc;
-			d = tmpd;
-			count = 0;
-			while (b < c) {
-				c /= 2;
-				count++;
-			}
-			while (a < d) {
-				d /= 2;
-				count++;
-			}
-			min = Math.min(min, count);
-
-			System.out.println(min);
+			int ans = solve(a, b, c, d);
+			ans = Math.min(ans, solve(b, a, c, d));
+			System.out.println(ans);
 		}
 		sc.close();
 	}
