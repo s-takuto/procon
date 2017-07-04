e054ecd43dfa88371e5313d6909370eff08e477f
s_takuto <s_takuto@cs.meiji.ac.jp>
2017-07-04 17:36:33 +0900

    ABC066 D

--- AtCoder/ABC066/D/Main.java
+++ AtCoder/ABC066/D/Main.java
@@ -4,7 +4,30 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
-	private static final int MOD = 1_000_000_000 + 7;
+	private static final long MOD = 1_000_000_000 + 7;
+
+	// 反復二乗法を用いる
+	public static long myPow(long a) {
+		long ans = 1;
+		long x = a;
+		long b = 1_000_000_000 + 5;
+
+		for(int i = 0; i < 32; i++) {
+			if((b & (1L << i)) != 0L) {
+				ans = (ans * x) % MOD;
+			}
+			x = (x * x) % MOD;
+		}
+
+		return ans % MOD;
+	}
+
+	public static long nck(int n, int k, long[] fact) {
+		if(n < k) {
+			return 0;
+		}
+		return (((fact[n] * myPow(fact[n - k])) % MOD) * myPow(fact[k])) % MOD;
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -12,39 +35,41 @@ public class Main {
 		int n = sc.nextInt();
 		int[] a = new int[n + 1];
 		int[] count = new int[n + 1];
+
 		int tar = 0;
+		int l = -1; int r = -1;
+
 		for(int i = 0; i < n + 1; i++) {
 			a[i] = sc.nextInt();
 			count[a[i]]++;
 			if(count[a[i]] == 2) {
-				tar = i + 1;
+				tar = a[i];
+				r = i + 1;
 			}
 		}
-		System.out.println(tar);
 
+		for(int i = 0; i < n + 1; i++) {
+			if(a[i] == tar) {
+				l = i + 1;
+				break;
+			}
+		}
+
+		// n+1までの階乗の計算
 		long[] fact = new long[n + 2];
 		fact[0] = 1;
-		fact[1] = 1;
-		for(int i = 2; i <= n + 1; i++) {
-			fact[i] = fact[i - 1] * i;
-			fact[i] %= MOD;
+		for(int i = 1; i <= n + 1; i++) {
+			fact[i] = (fact[i - 1] * i) % MOD;
 		}
 
-		System.out.println(Arrays.toString(fact));
-
-		for(int k = 1; k <= 1; k++) {
-			long tmp = (fact[n + 1 - k] * fact[k]) % MOD;
-			long comb1 = (fact[n + 1] / tmp) % MOD;
-			comb1 %= MOD;
-			System.out.println(comb1);
-			long comb2 = 0;
-			if(n + 1 - tar >= k - 1) {
-				tmp = (fact[n - tar - k + 2] * fact[k - 1]) % MOD;
-				comb2 = fact[n + 1 - tar] / tmp;
-				comb2 %= MOD;
-			}
-			//	System.out.print("comb1:" + comb1);
-			//	System.out.println(", comb2:" + comb2);
+		for(int k = 1; k <= n + 1; k++) {
+			//	long comb1 = (fact[n + 1] * ((myPow(fact[n + 1 - k]) * myPow(fact[k])) % MOD)) % MOD;
+			long comb1 = nck(n + 1, k, fact);
+			//	long comb2 = 0;
+			//	if(l + n - r >= k - 1) {
+			//		comb2 = (fact[l + n - r] * ((myPow(fact[l + n - r - k + 1]) * myPow(fact[k - 1])) % MOD)) % MOD;
+			//	}
+			long comb2 = nck(l + n -r, k - 1, fact);
 			System.out.println((comb1 - comb2) % MOD);
 		}
 	}
