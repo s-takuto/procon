0e0f6b08ae02d24cc3f59426980061bbe0f0e458
s_takuto <s_takuto@cs.meiji.ac.jp>
2017-07-06 14:45:34 +0900

    ABC0066 D accept

--- AtCoder/ABC066/D/Main.java
+++ AtCoder/ABC066/D/Main.java
@@ -8,22 +8,22 @@ public class Main {
 
 	// 反復二乗法を用いる
 	public static long myPow(long a) {
-		long ans = 1;
+		long ans = 1L;
 		long x = a;
-		long b = 1_000_000_000 + 5;
+		long b = 1_000_000_000 + 5L;
 
-		for(int i = 0; i < 32; i++) {
+		for(int i = 0; i < 64; i++) {
 			if((b & (1L << i)) != 0L) {
 				ans = (ans * x) % MOD;
 			}
 			x = (x * x) % MOD;
 		}
 
-		return ans % MOD;
+		return ans;
 	}
 
 	public static long nck(int n, int k, long[] fact) {
-		if(n < k) {
+		if(n < k || n < 0 || k < 0) {
 			return 0;
 		}
 		return (((fact[n] * myPow(fact[n - k])) % MOD) * myPow(fact[k])) % MOD;
@@ -37,7 +37,7 @@ public class Main {
 		int[] count = new int[n + 1];
 
 		int tar = 0;
-		int l = -1; int r = -1;
+		int l = 0; int r = 0;
 
 		for(int i = 0; i < n + 1; i++) {
 			a[i] = sc.nextInt();
@@ -57,20 +57,15 @@ public class Main {
 
 		// n+1までの階乗の計算
 		long[] fact = new long[n + 2];
-		fact[0] = 1;
+		fact[0] = 1L;
 		for(int i = 1; i <= n + 1; i++) {
 			fact[i] = (fact[i - 1] * i) % MOD;
 		}
 
 		for(int k = 1; k <= n + 1; k++) {
-			//	long comb1 = (fact[n + 1] * ((myPow(fact[n + 1 - k]) * myPow(fact[k])) % MOD)) % MOD;
 			long comb1 = nck(n + 1, k, fact);
-			//	long comb2 = 0;
-			//	if(l + n - r >= k - 1) {
-			//		comb2 = (fact[l + n - r] * ((myPow(fact[l + n - r - k + 1]) * myPow(fact[k - 1])) % MOD)) % MOD;
-			//	}
-			long comb2 = nck(l + n -r, k - 1, fact);
-			System.out.println((comb1 - comb2) % MOD);
+			long comb2 = nck(l + n - r, k - 1, fact);
+			System.out.println((comb1 - comb2 + MOD) % MOD);
 		}
 	}
 }
